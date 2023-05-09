108f81d490a19fa749e778886f689f4bfd00d029
nmsteve <nmsteve45@gmail.com>
2023-05-09 11:32:20 +0300

    info update: v1

--- src/components/Details.js
+++ src/components/Details.js
@@ -18,24 +18,23 @@ const Details = () => {
         </h1>
         <p className="text-black mt-2 xl:leading-loose mx-3 lg:leading-relaxed sm:leading-8 lg:mt-4 text-center xl:mt-6 xl:w-2/5 lg:w-3/5 sm:w-4/5 text-xs lg:text-xl sm:text-base sm:mx-auto">
           Owning one of these exclusive NFTs is not only a chance to add a
-          unique piece of art to your collection but also an opportunity to make
-          a difference in someone's life. With every purchase, you become a part
+          unique piece of art to your collection but also comes with added advantages. With every purchase, you become a part
           of Sidai Club, an exclusive membership that offers access to your 4k x
           4k file, airdrops, feature NFTs at a discounted price, and 1000 ERC20
           Sidai Tokens once complete.
         </p>
-        <p className="text-xs lg:text-xl sm:text-base xl:mt-16 lg:mt-12 text-center text-[#DC7171]">
+        <p className="text-xs lg:text-xl sm:text-base xl:mt-16 lg:mt-12 text-center text-[#DC7171] mt-2 m">
           Meet our Amazing Community
         </p>
-        <div className="flex flex-row gap-2 xs:gap-12 sm:mt-4 items-center justify-center ">
+        <div className="flex flex-row gap-2 xs:gap-12 sm:mt-4 items-center justify-center mt-2 ">
           <a href="/">
-            <FaDiscord className="text-black text-sm sm:text-xl  cursor-pointer hover:text-[#2A215F]" />
+            <FaDiscord className="text-black text-sm sm:text-xl  cursor-pointer hover:text-[#2A215F] mx-2" />
           </a>
           <a href="/">
-            <FaTwitter className="text-black text-sm sm:text-xl cursor-pointer hover:text-[#2A215F]" />
+            <FaTwitter className="text-black text-sm sm:text-xl cursor-pointer hover:text-[#2A215F] mx-2" />
           </a>
           <a href="/">
-            <FaInstagram className="text-black text-sm sm:text-xl cursor-pointer hover:text-[#2A215F]" />
+            <FaInstagram className="text-black text-sm sm:text-xl cursor-pointer hover:text-[#2A215F] mx-2" />
           </a>
         </div>
       </div>
