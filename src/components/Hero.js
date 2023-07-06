c66b12ed977bbec51ebe8af951850eed6b9ada58
nmsteve <nmsteve45@gmail.com>
2023-07-06 13:18:20 +0300

    Banner image working fine

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -5,16 +5,15 @@ import { icon } from '@fortawesome/fontawesome-svg-core/import.macro'
 
 const Hero = () => {
   return (
-
+   
+    
     <div className="relative">
-      {/* Video */}
-      <video className="w-screen absolute z-0 mb-50" autoplay loop>
-        <source
-          src="https://res.cloudinary.com/dk8epvq9b/video/upload/v1688635295/ENTITO_bhjy8n.mp4"
-          type="video/mp4"
-        />
-        {/* You can add additional video formats here */}
-      </video>
+      <img
+        className="w-screen z-0"
+        src="/vedio/ENTITO.gif"
+        alt="Images of the NFTs"
+      />
+
       
     </div>
   );
