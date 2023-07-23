bc46f2357d48590f8476e76787551ba7c7b63dc5
nmsteve <nmsteve45@gmail.com>
2023-07-23 07:32:07 +0300

    minting function ready and date update

--- src/components/1.Welcome.js
+++ src/components/1.Welcome.js
@@ -5,21 +5,38 @@ import { FaVolumeUp } from "react-icons/fa";
 import { Carousel } from 'react-responsive-carousel';
 import 'react-responsive-carousel/lib/styles/carousel.min.css';
 import { useMediaQuery } from 'react-responsive';
+import { mint, checkMinted, checkSupply } from "../connect/connectChain.js"
+const { ethereum } = window;
+
 
 const Welcome = () => {
-  
+
   const isSmallScreen = useMediaQuery({ maxWidth: 640 });
 
-  const [number, setNumber] = useState(0);
+  const [supply, setSupply] = useState(0);
+  const [minted, setMinted] = useState(0);
+
   const [countdown, setCountdown] = useState('');
-  
+
 
   const [quantity, setQuantity] = useState(1);
   const [price, setPrice] = useState(0.02)
 
+
+  const [connectedAddress, setConnectedAddress] = useState()
+
+  if (ethereum) {
+    ethereum.on('accountsChanged', () => {
+      setConnectedAddress(ethereum.selectedAddress)
+    })
+
+    window.ethereum.on('chainChanged', (chainId) => window.location.reload());
+
+  }
+
   const handleIncrement = () => {
     setQuantity(quantity + 1);
-    setPrice((quantity + 1)* 0.02);
+    setPrice((quantity + 1) * 0.02);
   };
 
   const handleDecrement = () => {
@@ -28,18 +45,18 @@ const Welcome = () => {
       setPrice((quantity - 1) * 0.02);
     }
   };
- 
-  
+
+
   const audio1Ref = useRef(null);
   const audio2Ref = useRef(null);
 
   const handleAudioClick = (audioRef) => {
-      audioRef?.current.play();
+    audioRef?.current.play();
   };
 
 
   useEffect(() => {
-    const countdownDate = new Date('July 29, 2023 00:00:00').getTime();
+    const countdownDate = new Date('august 26, 2023 00:00:00').getTime();
 
     const timer = setInterval(() => {
       const now = new Date().getTime();
@@ -65,6 +82,12 @@ const Welcome = () => {
     return () => clearInterval(timer);
   }, []);
 
+  useEffect(() => {
+    checkSupply(setSupply)
+    //console.log(supply)
+    checkMinted(setMinted)
+  }, [supply, minted, connectedAddress])
+
 
   return (
     <div className="relative max-w-screen">
@@ -79,14 +102,14 @@ const Welcome = () => {
         <p className="text-white font-serif text-center text-xs md:text-sm my-4 md:my-8">
           Celebrating Maasai Culture and Empowering Girls Through Education
         </p>
-      
+
         <div className="flex flex-col md:flex-row mx-4">
           <div className="md:w-2/5">
-            {isSmallScreen ?  
+            {isSmallScreen ?
               <div>
                 <img src="https://res.cloudinary.com/dk8epvq9b/image/upload/v1688800993/22_knlinz.jpg" alt="1" className="w-full" />
               </div>
-            :
+              :
               <Carousel
                 showArrows={true}
                 showIndicators={true}
@@ -110,63 +133,64 @@ const Welcome = () => {
                   <img src="https://res.cloudinary.com/dk8epvq9b/image/upload/v1688800896/25_bh5gax.jpg" alt="4" className="w-full" />
                 </div>
               </Carousel>
-              
+
             }
           </div>
 
           <div className="md:w-3/5 ml-10 mr-3 xs:mt-10 md:mt-0">
-              <p className="text-white  md:text-lg font-serif tracking-wide g-2 whitespace-pre-line leading-8 text-base text-left md:mx-auto">
-                Introducing the
-                <span className="text-[#DC7171]"> Entito Sidai Collection</span> - a
-                limited edition set of  <span className="text-[#DC7171]">2,000</span>  NFTs that embody the essence of Maasai
-                culture, diversity, and beauty. In the Maa language, "Entito (
-                |en’-tee-toh| )"
-                <FaVolumeUp
-                  className="text-sm inline cursor-pointer mx-2 text-[#DC7171]"
-                  onClick={() => handleAudioClick(audio1Ref)}
-                />
-                translates to a girl, and "Sidai ( |see’-da-ee| )"
-                <FaVolumeUp
-                  className="text-sm inline cursor-pointer mx-2 text-[#DC7171]"
-                  onClick={() => handleAudioClick(audio2Ref)}
-                />
-                means beautiful - and that's precisely what this collection
-                represents: the beauty of a Maasai girl adorned in traditional beaded
-                ornaments. Each NFT features a unique pattern inspired by the
-                intricate details of the ornaments, including a hair cover, forehead gear, earrings, and necklace.
-                <audio ref={audio1Ref}>
-                  <source src={"/audio/entito.ogg"} type="audio/ogg" />
-                </audio>
-                <audio ref={audio2Ref}>
-                  <source src={"/audio/sidai.ogg"} type="audio/ogg" />
-                </audio>
-              </p>
+            <p className="text-white  md:text-lg font-serif tracking-wide g-2 whitespace-pre-line leading-8 text-base text-left md:mx-auto">
+              Introducing the
+              <span className="text-[#DC7171]"> Entito Sidai Collection</span> - a
+              limited edition set of  <span className="text-[#DC7171]">2,000</span>  NFTs that embody the essence of Maasai
+              culture, diversity, and beauty. In the Maa language, "Entito (
+              |en’-tee-toh| )"
+              <FaVolumeUp
+                className="text-sm inline cursor-pointer mx-2 text-[#DC7171]"
+                onClick={() => handleAudioClick(audio1Ref)}
+              />
+              translates to a girl, and "Sidai ( |see’-da-ee| )"
+              <FaVolumeUp
+                className="text-sm inline cursor-pointer mx-2 text-[#DC7171]"
+                onClick={() => handleAudioClick(audio2Ref)}
+              />
+              means beautiful - and that's precisely what this collection
+              represents: the beauty of a Maasai girl adorned in traditional beaded
+              ornaments. Each NFT features a unique pattern inspired by the
+              intricate details of the ornaments, including a hair cover, forehead gear, earrings, and necklace.
+              <audio ref={audio1Ref}>
+                <source src={"/audio/entito.ogg"} type="audio/ogg" />
+              </audio>
+              <audio ref={audio2Ref}>
+                <source src={"/audio/sidai.ogg"} type="audio/ogg" />
+              </audio>
+            </p>
             <div>
-                <Slider number={number} />
-                <p className="text-white text-center text-lg font-bold md:text-2xl">
-                 {price} ETH
-                </p>
+              <Slider supply={supply} />
+              <p className="text-white text-center text-lg font-bold md:text-2xl">
+                {price} ETH
+              </p>
               <div className="flex justify-center gap-4 my-4">
                 <button className="bg-[#D08A21] hover:bg-[#744704] py-2 px-3 text-xl rounded-l-sm"
-                onClick={handleDecrement}>
+                  onClick={handleDecrement}>
                   <span className="text-white font-bold">-</span>
                 </button>
                 <div className=" py-2 px-3">
                   <span className="text-white text-xl font-bold">{quantity}</span>
                 </div>
                 <button className="bg-[#D08A21] hover:bg-[#744704] py-2 px-3 rounded-r-sm"
-                onClick={handleIncrement}>
+                  onClick={handleIncrement}>
                   <span className="text-white font-bold">+</span>
                 </button>
-                <button className="bg-[#D08A21] hover:bg-[#744704] py-2 flex items-center md:py-4 px-6 text-xl rounded-sm">
-                    <p className="text-white"> Mint </p>
+                <button className="bg-[#D08A21] hover:bg-[#744704] py-2 flex items-center md:py-4 px-6 text-xl rounded-sm"
+                  onClick={() => { mint(quantity, setMinted, setSupply) }}>
+                  <p className="text-white"> Mint </p>
                 </button>
               </div>
               <div className="flex justify-center gap-4 my-4">
                 {countdown !== 'Expired' && (
                   <>
                     <div className="text-white text-center my-4">
-                      <span className="font-bold">29TH JULY, 2023</span>
+                      <span className="font-bold">26TH AUG, 2023</span>
                     </div>
                     <div className="flex flex-col items-center">
                       <button className="text-white text-sm font-bold">{countdown.split(' ')[0]}</button>
@@ -188,11 +212,20 @@ const Welcome = () => {
                 )}
                 {countdown === 'Expired' && <span className="text-white">Expired</span>}
               </div>
+              {minted > 0 ?
+                <div className="flex justify-center gap-4 my-4">
+                  <p className="font-bold">Token Minted: {minted}</p>
+                </div> :
+                <div className="flex justify-center gap-4 my-4">
+                </div>
+              }
+
             </div>
           </div>
+        </div>
+
       </div>
-      
-      </div>
+
     </div>
   );
 };
--- src/connect/connectChain.js
+++ src/connect/connectChain.js
@@ -1,70 +1,16 @@
-import {ethers} from 'ethers'
+import { ethers } from 'ethers'
+import { abi } from './abi.js'
 
 const { ethereum } = window;
 
 //accessing provived changed to this
 let provider = new ethers.BrowserProvider(window.ethereum)
 
-// Contract ABI
-const contractABI = [
-    {
-        "inputs": [],
-        "name": "MAX_WAITLIST_SEATS",
-        "outputs": [
-            {
-                "internalType": "uint256",
-                "name": "",
-                "type": "uint256"
-            }
-        ],
-        "stateMutability": "view",
-        "type": "function"
-    },
-    {
-        "inputs": [],
-        "name": "joinWaitlist",
-        "outputs": [],
-        "stateMutability": "nonpayable",
-        "type": "function"
-    },
-    {
-        "inputs": [],
-        "name": "seatsFilled",
-        "outputs": [
-            {
-                "internalType": "uint256",
-                "name": "",
-                "type": "uint256"
-            }
-        ],
-        "stateMutability": "view",
-        "type": "function"
-    },
-    {
-        "inputs": [
-            {
-                "internalType": "address",
-                "name": "",
-                "type": "address"
-            }
-        ],
-        "name": "waitlisted",
-        "outputs": [
-            {
-                "internalType": "bool",
-                "name": "",
-                "type": "bool"
-            }
-        ],
-        "stateMutability": "view",
-        "type": "function"
-    }
-]
-
 // Deployed contract address
-// Goerli test net
-let contractAddress = "0xc91037E440ad3001726b0E701759eEBb6B6ef688";
-// Mainnet will be deployed on 18th JULY
+// Sepolia
+let sepoliaAddress = "0x5349d651f8c8c523d01917472a031c22F6cE7CDb";
+// Mainnet (will be deployed on 18th JULY)
+let mainnetAddress = '0x0000000000000000000000000000000000000000'
 
 export const checkIfConnected = async (setConnectedAddress) => {
 
@@ -77,7 +23,7 @@ export const checkIfConnected = async (setConnectedAddress) => {
         if (ethereum.selectedAddress) {
             await setConnectedAddress(ethereum.selectedAddress)
         }
-        
+
     } catch (error) {
         console.error("Failed to connect to MetaMask:", error);
     }
@@ -113,74 +59,157 @@ export const handleDisconnect = async (setConnectedAddress) => {
     }
 };
 
-// export const checkIfJoined = async (setWaitlisted) => {
-   
-//     const contract = new ethers.Contract(contractAddress, contractABI, provider);
-
-//     try {
-
-//         const waitlisted = await contract.waitlisted(ethereum.selectedAddress)
-//         if (waitlisted === true) {
-//            setWaitlisted(true)
-//         } else{
-//            setWaitlisted(false)
-//         }
-
-//     } catch (error) {
-//         console.error(error.message)
-//     }
-
-// }
-
-// export const joinWaitList = async () => {
-
-//     if (!ethereum) {
-//         alert("Please install MetaMask to connect.");
-//         return;
-//     }
-
-//     //connect if not connected
-//     await ethereum.request({ method: 'eth_requestAccounts' });
-
-//     //required to intract with a deployed contract
-//     const signer = await provider.getSigner(ethereum.selectedAddress)
-     
-//     const contract = new ethers.Contract(contractAddress, contractABI, signer);
-
-//     try {
-//         const transaction = await contract.joinWaitlist();
-//         await transaction.wait(1);
-//         console.log("Successfully joined the waitlist!");
-//     } catch (error) {
-//         console.error("Error joining the waitlist:", error);
-//         if (error.message.includes('Already on the waitlist')) {
-//             alert(`Transaction reverted: Already on the waitlist'`);
-//         } else {
-//             alert('Error joining the waitlist. Please try again later.');
-//         }
-//     }
-
-//     await checkSeatFilled()
-
-//     window.location.reload(false);
-// }
-
-// export const checkSeatFilled = async (setSeatsFilled) => {
-
-//     //connect to deployed contract
-//     const contract = new ethers.Contract(contractAddress, contractABI, provider)
-
-//     try {
-//         const seatsFilled = (await contract.seatsFilled()).toString()
-//         setSeatsFilled(seatsFilled)
-        
-        
-//     } catch (error) {
-//         console.error(error.message)
-        
-//     }
+export const mint = async (amount, setMinted, setSupply) => {
 
 
-// }
+    if (!ethereum) {
+        console.log("Please install MetaMask to connect.");
+        alert("Please install MetaMask to connect.")
+        return;
+    }
 
-    
+    const contractAddress = await checkConnectedChain()
+    console.log(contractAddress)
+
+    if (contractAddress === '0x0000000000000000000000000000000000000000') {
+        alert('Mint day on 5TH August')
+        return
+    }
+
+    if (contractAddress === 'x') {
+        alert('Unsupported Chain: Please switch to mainnet')
+        return
+    }
+
+    const bal = await checkBalance()
+    console.log(bal)
+
+    try {
+
+        //Get the signer
+        const signer = await provider.getSigner()
+
+        // Create an instance of the contract
+
+        const contract = new ethers.Contract(contractAddress, abi, signer);
+        //set price
+        const price = 0.02 * amount
+        console.log(price)
+
+        if (bal > price) {
+            const tx = await contract.publicMint(amount, { value: ethers.parseEther(price.toString()) })
+            await tx.wait(1)
+        } else {
+            alert('Insuffient Balance')
+        }
+
+
+    } catch (error) {
+        console.error(error)
+
+    }
+
+    await checkSupply(setSupply)
+    await checkMinted(setMinted)
+
+
+}
+
+export const checkMinted = async (setMinted) => {
+
+    if (!ethereum) {
+        console.log("Please install MetaMask to connect.");
+        return;
+    }
+
+    try {
+
+        // Create an instance of the contract
+        const contract = new ethers.Contract(sepoliaAddress, abi, provider);
+        const balance = await contract.balanceOf(ethereum.selectedAddress)
+        const balIndecimals = Number(balance)
+        console.log(balIndecimals)
+        setMinted(balIndecimals)
+
+    } catch (error) {
+
+        console.error(error)
+
+    }
+
+}
+
+export const checkSupply = async (setSupply) => {
+
+    if (!ethereum) {
+        console.log("Please install MetaMask to connect.");
+        return;
+    }
+
+    try {
+
+        // Create an instance of the contract
+        const contract = new ethers.Contract(sepoliaAddress, abi, provider);
+        const supply = await contract.totalSupply()
+        const formatedSupply = ethers.formatUnits(supply, 0)
+        console.log(formatedSupply)
+        setSupply(formatedSupply)
+
+    } catch (error) {
+
+        console.error(error)
+
+    }
+
+}
+
+export const checkBalance = async () => {
+
+    if (!ethereum) {
+        console.log("Please install MetaMask to connect.");
+        return;
+    }
+
+    try {
+
+        const bal = await provider.getBalance(ethereum.selectedAddress)
+        const formatedBal = ethers.formatEther(bal)
+        console.log(formatedBal)
+
+        return formatedBal
+
+    } catch (error) {
+        console.error(error)
+    }
+
+
+
+}
+
+export const checkConnectedChain = async () => {
+    if (!ethereum) {
+        alert("Please install MetaMask to connect.");
+        return;
+    }
+
+    try {
+
+        const chainId = await ethereum.request({ method: 'eth_chainId' });
+        console.log(chainId)
+
+        switch (chainId) {
+            //mainnet contract address
+            case "0x1":
+                return mainnetAddress;
+            //sepolia connected address
+            case '0xaa36a7':
+                return sepoliaAddress;
+
+            default:
+                return 'x';
+        }
+
+    } catch (error) {
+        console.error("Failed to get ChainId:", error);
+    }
+};
