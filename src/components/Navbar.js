652272abbb762eb700b7f255017912773e949b06
nmsteve <nmsteve45@gmail.com>
2023-08-22 17:03:56 +0300

    adding the contract button

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -16,16 +16,28 @@ const Navbar = ({connectedAddress, setConnectedAddress}) => {
           <img src={"/images/Layer_1.svg"} alt="logo" />
         </div>
         <div className="grid grid-cols-2  md:gap-6 items-center px-2 md:mr-16 ">
-          <div className="flex flex-row gap-4 md:gap-6 sm:gap-3 mr-8">
-            <a href="https://twitter.com/EntitoSidaiNFTs" target="_blank" rel="noopener noreferrer">
-              <FaTwitter className="text-white text-lg md:text-xl cursor-pointer hover:text-[#DC7171]" />
+          <div className="flex flex-row items-center gap-4 md:gap-6 sm:gap-3 mr-8">
+            <a href="https://twitter.com/EntitoSidaiNFTs" target="_blank" rel="noopener noreferrer"
+              className="flex items-center border-[#DC7171] hover:bg-gray-300 border-[1px] p-2 rounded-sm text-white text-center text-sm cursor-pointer">
+              X (Twitter)
             </a>
+            <button
+              className="flex items-center border-[#DC7171] hover:bg-gray-300 border-[1px] p-2 rounded-sm text-white text-center text-sm cursor-pointer"
+              onClick={() => {
+                window.open("https://arbiscan.io/address/0xc91037E440ad3001726b0E701759eEBb6B6ef688", "_blank");
+              }}
+            >
+              Contract
+            </button>
           </div>
+
+
+          
           {connectedAddress ? (
             <div className="relative inline-block">
               <div className="bg-gray-200 rounded-md p-2 flex items-center">
                 <AiOutlineUser className="mr-2" />
-                <p className="text-[#DC7171] text-sm">
+                <p className="text-white text-sm">
                   {formatAddress(connectedAddress)}
                 </p>
               </div>
@@ -46,7 +58,7 @@ const Navbar = ({connectedAddress, setConnectedAddress}) => {
                 }
                   }
               >
-                <p className="text-[#DC7171] text-center text-sm">Connect</p>
+                <p className="text-white text-center text-sm">Connect</p>
               </button>
            
           )}
