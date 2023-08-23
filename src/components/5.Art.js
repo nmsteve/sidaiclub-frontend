4d2f7b2e394ebc79c67e24ab3394a6cd6c7f81a6
nmsteve <nmsteve45@gmail.com>
2023-08-23 23:24:47 +0300

    add nftcander logo

--- src/components/5.Art.js
+++ src/components/5.Art.js
@@ -12,7 +12,7 @@ const Art = () => {
         <div className="flex flex-col md:flex-row mx-4 mt-10 mb-5">
 
         <div className="md:w-3/6">
-            <img src="https://res.cloudinary.com/dk8epvq9b/image/upload/v1688801495/Art_Banner_nuy4bc.png" />
+            <img src="https://res.cloudinary.com/dk8epvq9b/image/upload/v1688801495/Art_Banner_nuy4bc.png" alt="8 Arts"/>
           </div>
           
           <div className="md:w-3/6">
@@ -20,20 +20,40 @@ const Art = () => {
               <p className="text-white  md:text-lg font-serif tracking-wide g-2 whitespace-pre-line leading-8 text-base text-left md:mx-auto">
                 Entito Sidai NFT collection consists of 2000 unique NFTs, each containing 10 distinct traits, which make them one-of-a-kind. These NFTs are randomly named and organized, giving every individual an equal opportunity to own the rarest NFT.
               </p>
-              <p className="text-xs lg:text-xl sm:text-base xl:mt-16 lg:mt-12 text-center text-[#DC7171] mt-2 m">
-                Stay updated, follow us on Twitter
-              </p>
-              <div className="flex flex-row gap-2 xs:gap-12 xs:mt-1 items-center justify-center mt-2 sm:text-sm xs:text-xs font-bold">
-                <a href="https://twitter.com/EntitoSidaiNFTs" target="_blank" rel="noopener noreferrer" className="text-blue-500">
-                  @EntitoSidaiNFTs
-                </a>
+              <div className="flex flex-col md:flex-row">
+
+                <div className="flex flex-row md:w-3/6 mt-5">
+                  <div className="flex md:flex-row items-center xs:w-1/4">
+                    <p className="text-bold md:text-l text-white">as seen on</p>
+                  </div>
+                  <div className="xs:w-3/4 xs:pt-2">
+                    <a href="https://nftcalendar.io/event/entito-sidai-nfts/" target="_blank" rel="noopener noreferrer">
+                      <img className="w-auto h-16 md:w-auto md:h-auto mx-auto md:mx-0 border-[#DC7171] hover:bg-gray-300 md:border-[1px] rounded-sm cursor-pointer"
+                        src="https://res.cloudinary.com/dk8epvq9b/image/upload/v1692804207/nft_calendar_logo_vector_optimized_ezivc9.svg" alt="Logo" />
+                    </a>
+                  </div>
+                </div>
+
+                <div className="md:w-3/6">
+                  <p className="text-sm lg:text-xl sm:text-base xl:mt-16 lg:mt-12 text-center text-[#DC7171] mt-2 m">
+                    Stay updated, follow us on Twitter
+                  </p>
+                  <div className="flex flex-row gap-2 xs:gap-12 xs:mt-1 items-center justify-center mt-2 sm:text-sm xs:text-sm font-bold">
+                    <a href="https://twitter.com/EntitoSidaiNFTs" target="_blank" rel="noopener noreferrer" className="text-blue-500">
+                      @EntitoSidaiNFTs
+                    </a>
+                  </div>
+                  <p className="text-sm lg:text-xl sm:text-base xl:mt-16 lg:mt-12 text-center text-[#DC7171] mt-2 m">
+                    Reach us via Email
+                  </p>
+                  <div className="flex flex-row gap-2 xs:gap-12 xs:mt-1 items-center justify-center mt-2 sm:text-sm xs:text-xs font-bold text-white">
+                    entito@sidaiclub.com
+                    </div>
+                </div> 
+                
+
               </div>
-              <p className="text-xs lg:text-xl sm:text-base xl:mt-16 lg:mt-12 text-center text-[#DC7171] mt-2 m">
-                Reach us via Email
-              </p>
-              <div className="flex flex-row gap-2 xs:gap-12 xs:mt-1 items-center justify-center mt-2 sm:text-sm xs:text-xs font-bold text-white">
-                entito@sidaiclub.com
-              </div>
+
             </div>
           </div>
           
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,7 +2,9 @@ import React from "react";
 
 import { connectMetaMask,formatAddress} from '../connect/connectChain'
 
-import { AiOutlineUser, } from 'react-icons/ai';
+import { AiOutlineUser, AiOutlineTwitter } from 'react-icons/ai';
+
+import { FaFileContract } from 'react-icons/fa'
 
 
 const Navbar = ({connectedAddress, setConnectedAddress}) => {
@@ -13,24 +15,22 @@ const Navbar = ({connectedAddress, setConnectedAddress}) => {
         <div className="w-2/5">
           <img src={"/images/Layer_1.svg"} alt="logo" />
         </div>
-        <div className="grid grid-cols-2  md:gap-6 items-center px-2 md:mr-16 ">
+        <div className="grid grid-cols-2  md:gap-6 items-center px-2 md:mr-8 ">
           <div className="flex flex-row items-center gap-4 md:gap-6 sm:gap-3 mr-8">
             <a href="https://twitter.com/EntitoSidaiNFTs" target="_blank" rel="noopener noreferrer"
-              className="flex items-center border-[#DC7171] hover:bg-gray-300 border-[1px] p-2 rounded-sm text-white text-center text-sm cursor-pointer">
-              X (Twitter)
+              className="flex items-center border-[#DC7171] hover:bg-gray-300 border-[1px] p-2 rounded-sm text-white text-center md:text-xl text-sm cursor-pointer">
+              <AiOutlineTwitter/>
             </a>
             <button
-              className="flex items-center border-[#DC7171] hover:bg-gray-300 border-[1px] p-2 rounded-sm text-white text-center text-sm cursor-pointer"
+              className="flex items-center border-[#DC7171] hover:bg-gray-300 border-[1px] p-2 rounded-sm text-white text-center md:text-xl text-sm cursor-pointer"
               onClick={() => {
                 window.open("https://arbiscan.io/address/0xc91037E440ad3001726b0E701759eEBb6B6ef688", "_blank");
               }}
             >
-              Contract
+              <FaFileContract/>
             </button>
           </div>
 
-
-          
           {connectedAddress ? (
             <div className="relative inline-block">
               <div className="bg-gray-200 rounded-md p-2 flex items-center">
@@ -48,17 +48,17 @@ const Navbar = ({connectedAddress, setConnectedAddress}) => {
             </div>
             
           ) : (
-              
+              <div className="flex justify-end">
               <button
-                className="border-[#DC7171] hover:bg-gray-300 border-[1px] p-2 rounded-sm"
+                className="border-[#DC7171] hover:bg-gray-300 border p-2 rounded-sm"
                 onClick={() => {
                   connectMetaMask(setConnectedAddress)
                 }
                   }
               >
-                <p className="text-white text-center text-sm">Connect</p>
+                <p className="text-white md:text-base text-sm">Connect</p>
               </button>
-           
+              </div>
           )}
         </div>
       </div>
